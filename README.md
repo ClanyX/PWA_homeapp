f36f9c44d7de38cba9fdec2a8738dd28072cdbd6
ClanyX <sandik.filip@gmail.com>
2024-10-13 00:40:45 +0200

    Added service-worker

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PWA_homeapp
 Test application of PWA system. This web will be online.
+Web domain: https://pwatest1012.netlify.app/
---
+++ service-worker.js
@@ -0,0 +1,38 @@
+//Registration of serivice worker
+if('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(registration => {
+            console.log('Service Worker registration with scope:', registration.scope);
+        }, err => {
+            console.log('Service Worker not registrate:', err);
+        });
+    });
+}
+
+//Instalation of service worker
+self.addEventListener('install', event => {
+    console.log('Service Worker is instalating...');
+    event.waitUntil(
+        caches.open('my-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                'index_style.css'
+            ]);
+        })
+    );
+});
+
+//Aktivation of Service Worker
+self.addEventListener('activate', event => {
+    console.log('Service Worker was aktivated');
+});
+
+//Capturing requirements
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
